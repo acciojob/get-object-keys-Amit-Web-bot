ceb2f7c38e1fa09186a7880d0630aad42ce3b70e
Amit <84635639+Amit-Web-bot@users.noreply.github.com>
2024-01-10 13:33:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //your JS code here. If required.
-const student = {
+const student1 = {
 	name: "John", 
 	age: 30, 
 	city: "New York"
+}
+const student2 = {
+	name: "John",
 }
 Object.prototype.getKeys = function () {
 	return Object.keys(student)
